50a8e8fe4194890ec094619e58cebe110bdef902
TAISHI <taishi.nojima@yale.edu>
2015-04-11 16:46:02 -0400

    cancel should work

--- iDuino/ExampleViewController.swift
+++ iDuino/ExampleViewController.swift
@@ -9,76 +9,61 @@
 import UIKit
 
 class ExampleViewController: UIViewController {
+
+  
+  var timerTXDelay: NSTimer?
+  var allowTX = true
+  var currentInstruction: UInt8 = 0
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
+    // Start the Bluetooth discovery process
+    btDiscoverySharedInstance
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+  }
+  
+  @IBAction func someButtonPressed(sender: UISlider) {
+    // Call sendByteString with new instruction here!
     
-    var timerTXDelay: NSTimer?
-    var allowTX = true
-    var currentInstruction: UInt8 = 0
+
+    var request = BluetoothRequest.bluetoothRequestWithType(.Servo)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Watch Bluetooth connection
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("connectionChanged:"), name: BLEServiceChangedStatusNotification, object: nil)
-        
-        // Start the Bluetooth discovery process
-        btDiscoverySharedInstance
+    //said component does not exist
+    if request.componentType == .None {
+      // out of assignable pins, act accordingly
     }
     
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: BLEServiceChangedStatusNotification, object: nil)
-    }
     
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        self.stopTimerTXDelay()
+    // Give Request Appropriate Value
+    // e.g. request.value = .On
+    
+    sendByteString(request.generateByteString())
+  }
+  
+  func sendByteString(byteString: UInt8) {
+    
+    // Is the instruction already running?
+    if byteString == currentInstruction {
+      return
     }
     
     @IBAction func someButtonPressed(sender: UISlider) {
         // Call sendByteString with new instruction here!
     }
     
-    func sendByteString(byteString: UInt8) {
-        // Valid position range: 0 to 180
-        
-        if !self.allowTX {
-            return
-        }
-        
-        // Is the instruction already running?
-        if byteString == currentInstruction {
-            return
-        }
-        
-        
-        // Send position to BLE Shield (if service exists and is connected)
-        if let bleService = btDiscoverySharedInstance.bleService {
-            bleService.writePosition(byteString)
-            currentInstruction = byteString;
-            
-            // Start delay timer
-            self.allowTX = false
-            if timerTXDelay == nil {
-                timerTXDelay = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("timerTXDelayElapsed"), userInfo: nil, repeats: false)
-            }
-        }
+    // Send bytes5tring to BLE Shield (if service exists and is connected)
+    if let bleService = btDiscoverySharedInstance.bleService {
+      bleService.writePosition(byteString)
+      currentInstruction = byteString;
+      
     }
-    
-    func timerTXDelayElapsed() {
-        self.allowTX = true
-        self.stopTimerTXDelay()
-        self.sendByteString(currentInstruction)
-    }
-    
-    func stopTimerTXDelay() {
-        if self.timerTXDelay == nil {
-            return
-        }
-        
-        timerTXDelay?.invalidate()
-        self.timerTXDelay = nil
-    }
-    
+  }
+  
 }
 
