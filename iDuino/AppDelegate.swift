24f000b29ddba6a0a4a988a88180cca4a8cae6c0
TAISHI <taishi.nojima@yale.edu>
2015-04-12 03:38:41 -0400

    status

--- iDuino/AppDelegate.swift
+++ iDuino/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
+    UIApplication.sharedApplication().statusBarStyle = .LightContent
 
     return true
   }
